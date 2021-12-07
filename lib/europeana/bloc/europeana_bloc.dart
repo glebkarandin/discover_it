57c518271671f9063e0ac2f8cab3c0b01599c166
gleb.karandin <alohomorAtuI891>
2021-12-07 18:34:11 +0300

    BLoC init

--- lib/europeana/bloc/europeana_bloc.dart
+++ lib/europeana/bloc/europeana_bloc.dart
@@ -1,3 +1,11 @@
-import '';
+import 'package:discover_it/europeana/bloc/europeana_events.dart';
+import 'package:discover_it/europeana/bloc/europeana_state.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class EuropeanaBloc {}
+class EuropeanaBloc extends Bloc<EuropeanaEvent, EuropeanaState> {
+  EuropeanaBloc(EuropeanaState initialState) : super(initialState) {
+    on<EuropeanaEvent>((event, emit) {
+
+    });
+  }
+}
